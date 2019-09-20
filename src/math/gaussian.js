d360312f083274e5da2cbed8fbe3b7af278c6335
Sidaroth <sidaroth@protonmail.com>
2019-09-20 18:39:14 +0200

    Refactored gaussian to be more modern.

--- src/math/gaussian.js
+++ src/math/gaussian.js
@@ -1,28 +1,35 @@
 /**
- * returns a gaussian random function with the given mean and stdev.
- * https://stackoverflow.com/a/35599181 ==> https://www.wikiwand.com/en/Marsaglia_polar_method
+ * Returns a gaussian random function with the given mean and stdev.
+ * https://www.wikiwand.com/en/Marsaglia_polar_method
+ * Consider implement ziggurat algorithm instead as it's faster.
  */
-export default function gaussian(mean, stdev) {
-    var y2;
-    var use_last = false;
-    return function() {
-        var y1;
-        if (use_last) {
-            y1 = y2;
-            use_last = false;
-        } else {
-            var x1, x2, w;
-            do {
-                x1 = 2.0 * Math.random() - 1.0;
-                x2 = 2.0 * Math.random() - 1.0;
-                w = x1 * x1 + x2 * x2;
-            } while (w >= 1.0);
-            w = Math.sqrt(-2.0 * Math.log(w) / w);
-            y1 = x1 * w;
-            y2 = x2 * w;
-            use_last = true;
-        }
+const gaussianGen = function gaussianGen(mean, stdev) {
+    let hasSpare = false;
+    let spare;
 
-        return mean + stdev * y1;
+    return () => {
+        if (hasSpare) {
+            hasSpare = false;
+            return mean + stdev * spare;
+        } else {
+            let num1;
+            let num2;
+            let mag;
+
+            do {
+                num1 = 2.0 * Math.random() - 1.0;
+                num2 = 2.0 * Math.random() - 1.0;
+                mag = num1 * num1 + num2 * num2;
+            } while (mag >= 1.0 || mag === 0);
+
+            mag = Math.sqrt((-2.0 * Math.log(mag)) / mag);
+            spare = num2 * mag;
+            hasSpare = true;
+
+            const generated = num1 * mag;
+            return mean + stdev * generated;
+        }
     };
-}
+};
+
+export default gaussianGen;
